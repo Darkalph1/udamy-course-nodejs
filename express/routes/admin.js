887b42a79f457d9157d90e7ce1645deb31c39d91
alan joseph <alanjosephpulpally@gmail.com>
2022-11-01 07:05:20 +0530

    add util/path.js for get root dir. so we can wright more cleaner code for admin.js, shop.js

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../util/path');
 
 
 const router = express.Router();
@@ -8,7 +9,7 @@ const router = express.Router();
 // it is reached '/admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     res.status(200);
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')); //to get path of the html file
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html')); //to get path of the html file
 });
 
 // it is reached '/admin/add-product => POST
