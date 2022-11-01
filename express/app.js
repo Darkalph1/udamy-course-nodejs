098c28b17655b548dd7d4892716544c21f322c94
alan joseph <alanjosephpulpally@gmail.com>
2022-11-01 16:16:12 +0530

    small change (merge conflice)

--- express/app.js
+++ express/app.js
@@ -13,12 +13,7 @@ const adminData = require('./routes/admin');
 const shopRouters = require('./routes/shop');
 const errorRouter = require('./routes/error404');
 
-<<<<<<< HEAD
 app.use(express.static(__dirname + '/public'));
-=======
-app.use(express.static(path.join(__dirname, 'public')));
-
->>>>>>> 124cb995b61954f42b30fbb7bcdb38b86b639f66
 
 app.use(bodyParser.urlencoded({extended: false}));
 
