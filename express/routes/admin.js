458d4eb7a3d6c09a8c7badfa30c5ce8a62e27773
alan joseph <alanjosephpulpally@gmail.com>
2022-11-01 06:59:28 +0530

    serving HTML page to client

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 
 const router = express.Router();
@@ -7,7 +8,7 @@ const router = express.Router();
 // it is reached '/admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     res.status(200);
-    res.send("<form action='/product' method='post'><input type='text' name='title'> <button type='submit'>submit</button></form>");
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')); //to get path of the html file
 });
 
 // it is reached '/admin/add-product => POST
--- express/routes/error404.js
+++ express/routes/error404.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const path = require('path');
+
 router.use((req, res, next) =>{
     res.status(404);
-    res.send("<h1>page not found!!</h1>");
+    //res.send("<h1>page not found!!</h1>");
+    res.sendFile(path.join(__dirname, '../', 'views', 'error.html')); //to get path of the html file
 });
 
 module.exports = router;
