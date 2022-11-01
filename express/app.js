124cb995b61954f42b30fbb7bcdb38b86b639f66
alan joseph <alanjosephpulpally@gmail.com>
2022-11-01 10:35:49 +0530

    accessing static  css files to our html code

--- express/app.js
+++ express/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+//const rootDir = require('./util/path');
 
 const app = express();
 
 const adminRouters = require('./routes/admin');
 const shopRouters = require('./routes/shop');
 const errorRouter = require('./routes/error404');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(bodyParser.urlencoded({extended: false}));
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -6,8 +6,6 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    //res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
